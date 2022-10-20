1cd38f7576adbca4af9db4a4a3bbc9b2d5ec0af6
D VAMSIDHAR <vamsidhard2002@gmail.com>
2022-10-20 12:51:37 +0530

    Add files via upload

---
+++ NORMALITY TESTS/Anderson-Darling Test.py
@@ -0,0 +1,10 @@
+from scipy.stats import anderson
+data = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+result = anderson(data)
+print('stat=%.3f' % (result.statistic))
+for i in range(len(result.critical_values)):
+	sl, cv = result.significance_level[i], result.critical_values[i]
+	if result.statistic < cv:
+		print('Probably Gaussian at the %.1f%% level' % (sl))
+	else:
+		print('Probably not Gaussian at the %.1f%% level' % (sl))
